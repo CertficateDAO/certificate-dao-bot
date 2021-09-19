9be2b5eb56184dab4f46f9032f29216b1e40390d
李树彬 <koreQAQ@foxmail.com>
2021-09-19 23:18:31 +0800

    Feat add operations bot

--- script/start.sh
+++ script/start.sh
@@ -1,2 +1,8 @@
-export WECHATY_PUPPET_SERVICE_TOKEN=puppet_paimon_391415d7-9843-4ea6-974c-5eae14c75216
-./venv/bin/python3 ./src/wechat-bot.py
+#git clone https://github.com/wechaty/python-wechaty-getting-started
+#cd python-wechaty-getting-started
+#pip3 install -r requirements.txt
+#export WECHATY_PUPPET=wechaty-puppet-service
+#export WECHATY_PUPPET_SERVICE_TOKEN=16f1a001-f5bc-4e56-b2e7-411ae9c7c318
+#export WECHATY_TOKEN=16f1a001-f5bc-4e56-b2e7-411ae9c7c318
+#python3 examples/ding-dong-bot.py
+git clone https://github.com/CertficateDAO/certificate-dao-bot
